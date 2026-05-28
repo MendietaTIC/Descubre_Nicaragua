928a0bf79b60795b415eda2aad912529fe236f19
MendietaTIC <146373911+MendietaTIC@users.noreply.github.com>
2026-05-27 18:31:58 -0600

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -1,40 +1,48 @@
 exports.handler = async (event) => {
 
-    const API_KEY = process.env.GEMINI_API_KEY;
-
     try {
+
+        const API_KEY = process.env.GEMINI_API_KEY;
 
         const body = JSON.parse(event.body);
 
         const response = await fetch(
-            `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent`,
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
             {
                 method: "POST",
+
                 headers: {
                     "Content-Type": "application/json"
                 },
+
                 body: JSON.stringify(body)
             }
         );
 
         const data = await response.json();
 
-        console.log("RESPUESTA GEMINI:", JSON.stringify(data));
+        console.log(JSON.stringify(data));
 
         return {
             statusCode: 200,
+
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
             },
+
             body: JSON.stringify(data)
         };
 
     } catch (error) {
 
-        console.log(error);
-
         return {
             statusCode: 500,
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
             body: JSON.stringify({
                 error: error.message
             })
